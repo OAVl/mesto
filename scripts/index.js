49f2fed5da0d09919dac54ae2d2f2eff38d31d4b
Anastasiya Oborina <yasnastya2010@gmail.com>
2021-10-16 23:46:55 +0300

    feat: написать логику для открытия и закрытия модального окна, и сохранения данных

---
+++ scripts/index.js
@@ -0,0 +1,31 @@
+let popup = document.querySelector('.popup');
+let buttonOpenPopup = document.querySelector('.profile__button-edit');
+let buttonClosePopup = popup.querySelector('.popup__button-close');
+let popupForm = popup.querySelector('.popup__form');
+let nameInput = popupForm.querySelector('.popup__field_name');
+let jobInput = popupForm.querySelector('.popup__field_job');
+let saveButton = popup.querySelector('.popup__button');
+
+buttonOpenPopup.addEventListener('click', function() {
+    popup.classList.add('popup_opened');
+});
+
+buttonClosePopup.addEventListener('click', function() {
+    popup.classList.remove('popup_opened');
+});
+
+function formSubmitHandler (evt) {
+    evt.preventDefault();
+
+    let profileName = document.querySelector('.profile__name');
+    let profileJob = document.querySelector('.profile__description');
+
+    profileName.textContent = `${nameInput.value}`;
+    profileJob.textContent = `${jobInput.value}`;
+
+    saveButton.addEventListener('click', function() {
+        popup.classList.remove('popup_opened');
+    });
+}
+
+popupForm.addEventListener('submit', formSubmitHandler);
