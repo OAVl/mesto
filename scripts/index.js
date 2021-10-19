185e42cee5274ecea0918efc4d220054240254e8
Anastasiya Oborina <yasnastya2010@gmail.com>
2021-10-19 18:30:45 +0300

    fix: исправить по ревью

--- scripts/index.js
+++ scripts/index.js
@@ -2,30 +2,34 @@ let popup = document.querySelector('.popup');
 let buttonOpenPopup = document.querySelector('.profile__button-edit');
 let buttonClosePopup = popup.querySelector('.popup__button-close');
 let popupForm = popup.querySelector('.popup__form');
-let nameInput = popupForm.querySelector('.popup__field_name');
-let jobInput = popupForm.querySelector('.popup__field_job');
-let saveButton = popup.querySelector('.popup__button');
+let nameInput = popupForm.querySelector('.popup__field_input_name');
+let jobInput = popupForm.querySelector('.popup__field_input_job');
+let profileName = document.querySelector('.profile__name');
+let profileJob = document.querySelector('.profile__description');
 
-buttonOpenPopup.addEventListener('click', function() {
+function popupOpen () {
+
+    nameInput.value = profileName.textContent;
+    jobInput.value = profileJob.textContent;
+
     popup.classList.add('popup_opened');
-});
+}
 
-buttonClosePopup.addEventListener('click', function() {
+function popupClose () {
     popup.classList.remove('popup_opened');
-});
+}
 
-saveButton.addEventListener('click', function() {
-    popup.classList.remove('popup_opened');
-});
+buttonOpenPopup.addEventListener('click', popupOpen);
+
+buttonClosePopup.addEventListener('click', popupClose);
 
 function formSubmitHandler (evt) {
     evt.preventDefault();
 
-    let profileName = document.querySelector('.profile__name');
-    let profileJob = document.querySelector('.profile__description');
+    profileName.textContent = nameInput.value;
+    profileJob.textContent = jobInput.value;
 
-    profileName.textContent = `${nameInput.value}`;
-    profileJob.textContent = `${jobInput.value}`;
+    popupClose();
 }
 
 popupForm.addEventListener('submit', formSubmitHandler);
