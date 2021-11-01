4bbb9c862bbcf964326fcd6f264f0f9095fefb62
Anastasiya Oborina <yasnastya2010@gmail.com>
2021-11-02 02:03:35 +0300

    fix: исправить название классов по БЭМ

--- scripts/index.js
+++ scripts/index.js
@@ -8,7 +8,7 @@ const profileName = document.querySelector('.profile__name');
 const profileJob = document.querySelector('.profile__description');
 const elementList = document.querySelector('.elements');
 const templateElement = document.querySelector('.template-element');
-const popupAddElement = document.querySelector('.popup__element');
+const popupAddElement = document.querySelector('.popup_element');
 const elementAddButton = document.querySelector('.profile__button-add');
 const closeButtonPopupAddElement = popupAddElement.querySelector('.popup__button-close');
 const elementTitleInput = popupAddElement.querySelector('.popup__field_input_name');
@@ -105,11 +105,11 @@ function popupOpen() {
 }
 
 function openedPopupAddElement() {
-  popupAddElement.classList.add('popup__element_open');
+  popupAddElement.classList.add('popup_element_open');
 }
 
 function closePopupAddElement() {
-  popupAddElement.classList.remove('popup__element_open');
+  popupAddElement.classList.remove('popup_element_open');
 }
 
 function popupClose() {
